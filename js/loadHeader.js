b4749a29877fec8052b5795c859ed7b7bb88dcb7
Maxwell <mohbrookes@hotmail.ca>
2025-04-28 18:57:53 -0400

    3

---
+++ js/loadHeader.js
@@ -0,0 +1,7 @@
+fetch('./modular/header.html')
+  .then(response => response.text())
+  .then(data => {
+    const headFragment = document.createRange().createContextualFragment(data);
+    document.querySelector('header').appendChild(headFragment);
+  })
+  .catch(error => console.error('Error loading header:', error));
